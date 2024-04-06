86026c92cf0e1a937bdaa5fde5d44a8db7843102
CryptoLoM <153677367+CryptoLoM@users.noreply.github.com>
2024-04-06 12:53:26 +0200

    Update bubbletest.py

--- bubbletest.py
+++ bubbletest.py
@@ -14,22 +14,11 @@ class TestBubbleSort(unittest.TestCase):
         self.assertRaises(TypeError, bubbleSort, ['Vasya', 1, 2, 3, 3j], 4, 35, 12)
         self.assertRaises(TypeError, bubbleSort, [[]])
 
-    def test_input_limit(self):
-        input_limit = 10
-        arr = [1] * (input_limit + 1)
-        with self.assertRaises(Exception):
-            bubbleSort(arr)
-
-    def test_values_Min_and_Max(self):
-        with self.assertRaises(ValueError) as i:
-            bubbleSort([1000001, -1001])
-        self.assertEqual('value amount exceeds max', i.exception.args[0])
-
-
     def test_sorted_array(self):
-        not_sorted_list = [2, -24, 13839, 273, -383, 82, 3, 2, 5]
+        arr = [2, -24, 13839, 273, -383, 82, 3, 2, 5]
+        sorted_list = bubbleSort(arr)
         expected_sorted_list = [-383, -24, 2, 2, 3, 5, 82, 273, 13839]
-        self.assertEqual(bubbleSort(not_sorted_list), expected_sorted_list )
+        self.assertEqual(sorted_list, expected_sorted_list)
 
 
 if __name__ == "__main__":
