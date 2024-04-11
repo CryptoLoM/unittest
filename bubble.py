7f4d9698926bba6d3d8d8b78680d3a393f3a2072
CryptoLoM <153677367+CryptoLoM@users.noreply.github.com>
2024-04-11 15:23:16 +0200

    Update bubble.py

--- bubble.py
+++ bubble.py
@@ -1,14 +1,24 @@
 def bubbleSort(array):
-    # Test for types
-    for el in array:
-        if not isinstance(el, int):
-            raise TypeError('Unexpected types')
+    filtered_data = [x for x in array if isinstance(x, (int, float))]  # Filter out strings
 
-    n = len(array)
+    for item in filtered_data:
+        if not isinstance(item, (int, float)):
+            raise TypeError("List contains non-integer and non-float elements.")
 
-    # Sorting algorithm
-    for i in range(n - 1):
-        for j in range(0, n - i - 1):
-            if array[j] > array[j + 1]:
-                array[j], array[j + 1] = array[j + 1], array[j]  # Swap elements
-    return array
+    if not filtered_data:
+        print('Nothing to sort. The list returns with the initial view:', array)
+        return array
+
+    is_sorted = all(filtered_data[i] <= filtered_data[i + 1] for i in range(len(filtered_data) - 1))
+
+    if is_sorted:
+        print("The list is already sorted:", filtered_data)
+        return filtered_data
+
+    # Bubble sort algorithm
+    for i in range(len(filtered_data) - 1):
+        for j in range(0, len(filtered_data) - i - 1):
+            if filtered_data[j] > filtered_data[j + 1]:
+                filtered_data[j], filtered_data[j + 1] = filtered_data[j + 1], filtered_data[j]  # Swap elements
+    return filtered_data
+
