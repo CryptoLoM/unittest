78d3b38bc303eababb18c847ca237de93be2e0ae
CryptoLoM <153677367+CryptoLoM@users.noreply.github.com>
2024-04-12 01:33:52 +0200

    Update bubbletest.py

--- bubbletest.py
+++ bubbletest.py
@@ -1,51 +1,46 @@
 import unittest
-import subprocess
+import sys
+import io
 from bubble import bubbleSort
-
+import bubble as b
 
 class TestBubbleSort(unittest.TestCase):
 
-    def test_exit_code_success(self):
-        # Arrange
-        list = [1, 4, -4, 5]
+    def test_succes(Askim):
+        nums_exit_code_0 = [2, 3, 1, 2, 1, 3.1]
+        exit_code_0 = b.main(nums_exit_code_0)[0]
+        Askim.assertEqual(exit_code_0, 0)
 
-        # Act
-        exit_code = subprocess.call("bubble.py", shell=True)
+    def test_succes_after_correction(self):
+        nums_exit_code_1 = ['Evgeniy Vitaliyovych', 3, 1, 'sdmksb', 1, 3.1]
+        exit_code_1 = b.main(nums_exit_code_1)  # Отримання коду виходу
+        self.assertEqual(exit_code_1, 1)
+        nums_exit_code_1_str = "1 1 3 3.1 "
+        exit_code_1_str = b.main(nums_exit_code_1_str)  # Отримання коду виходу
+        self.assertEqual(exit_code_1_str, 1)
 
-        # Assert
-        self.assertEqual(exit_code, 0)
-
-    def test_exit_code_success_after_correction(self):
-        # Arrange
-        list1 = ['afsdg', 1.23, 2, -1]
-
-        # Act
-        exit_code = subprocess.call("bubble.py", shell=True)
-
-        # Assert
-        self.assertNotEqual(exit_code, 1)
-
-
-    def test_exit_code_failure(self):
-        # Arrange
-        list3 = ['jfjshf','hgsdf', 'dnjd']
-
-        # Act
-        exit_code = subprocess.call("bubble.py", shell=True)
-
-        # Assert
-        self.assertNotEqual(exit_code, 2)
-
+    def test_failure(Sensei):
+        nums_exit_code_2 = ['oh my cookish','ya','ni ne tu']
+        exit_code_2 = b.fail(nums_exit_code_2)  # Отримання коду виходу
+        Sensei.assertEqual(exit_code_2, 2)
 
     def test_empty(self):
         arr = []
         expected_arr = []
         self.assertEqual(bubbleSort(arr), expected_arr)
 
+
     def test_one_element(self):
         array = [21]
         expected_result = [21]
         self.assertEqual(bubbleSort(array), expected_result)
+
+
+    def test_same_elements(self):
+        massive = [3, 1, 3, 2, 3, 2]
+        expected_massive = [1, 2, 2, 3, 3, 3]
+        sort_massive = bubbleSort(massive)
+        self.assertEqual(bubbleSort(massive), expected_massive)
 
 
     def test_float_elements(self):
@@ -54,10 +49,12 @@ class TestBubbleSort(unittest.TestCase):
         sorted_arr = bubbleSort(arr)
         self.assertEqual(bubbleSort(arr), expected_arr)
 
+
     def test_negative_elements(self):
         array = [-21, 123, -8, 2, -23]
         expected_res = [-23, -21, -8, 2, 123]
         self.assertEqual(bubbleSort(array), expected_res)
+
 
     def test_valid_array(self):
         arr = [24, 13839, 273, 383, 82, 3, 2, 5]
@@ -65,14 +62,17 @@ class TestBubbleSort(unittest.TestCase):
         expected_sorted_list = [2, 3, 5, 24, 82, 273, 383, 13839]
         self.assertEqual(sorted_list, expected_sorted_list)
 
+
     def test_not_full_valid_array(self):
         arr = ['kadgkh', 71347, 4673]
         sorted_array = bubbleSort(arr)
         expected = [4673, 71347]
         self.assertEqual(sorted_array, expected)
 
+
     def test_not_valid_array(self):
           self.assertRaises(TypeError,bubbleSort,[2j,'knjd'],'ncdn')
+
 
     def test_already_sorted_list(self):
         # Arrange
